5ec9286a4a80dd0cf6f4aed67f8061007a206e1c
Juan <pablowechg@gmail.com>
2022-04-27 01:29:24 -0500

    formatted with pep8

--- src/data-cleaning.py
+++ src/data-cleaning.py
@@ -11,7 +11,8 @@ def clean_cedis(cedis_db_path: str):
     cedis_df['DOMICILIO'] = cedis_df['DOMICILIO'].str.strip()
     cedis_df['COLONIA'] = cedis_df['COLONIA'].str.strip()
     cedis_df['CIUDAD'] = cedis_df['CIUDAD'].str.strip()
-    cedis_df[['coord1', 'coord2']] = cedis_df['Coordenadas'].str.split(',', expand=True)
+    cedis_df[['coord1', 'coord2']] = cedis_df['Coordenadas'].str.split(
+        ',', expand=True)
 
     # Save to csv
     cedis_df.to_csv(PROCESSED_DATA_PATH + 'cedis.csv', index=False)
@@ -37,35 +38,41 @@ def clean_deliveries(deliveries_db_path: str):
     deliveries_df = deliveries_df.drop(columns=['Distribución'])
 
     # Converting dates to datetime
-    deliveries_df['fechaventa'] = pd.to_datetime(deliveries_df['fechaventa'], infer_datetime_format=True)
-    deliveries_df['fechaenrutada'] = pd.to_datetime(deliveries_df['fechaenrutada'], infer_datetime_format=True)
+    deliveries_df['fechaventa'] = pd.to_datetime(
+        deliveries_df['fechaventa'], infer_datetime_format=True)
+    deliveries_df['fechaenrutada'] = pd.to_datetime(
+        deliveries_df['fechaenrutada'], infer_datetime_format=True)
 
     # Keep only the latest deliveries
-    deliveries_df = deliveries_df[deliveries_df['fechaenrutada'] == deliveries_df['fechaenrutada'].max()]
+    deliveries_df = deliveries_df[deliveries_df['fechaenrutada']
+                                  == deliveries_df['fechaenrutada'].max()]
     deliveries_df.dropna(inplace=True)
 
     # Proposed full address
     deliveries_df['full_address'] = deliveries_df['nombrecalle'].str.strip().str.lower() + ',' + \
-                                deliveries_df['numerodecasa'].astype(int).astype(str).str.strip() + ',' + \
-                                deliveries_df['departamento'].str.strip() + ',' + \
-                                deliveries_df['nombreciudad'].str.strip().str.lower() + ',' + \
-                                deliveries_df['num_codigopostal'].astype(int).astype(str).str.strip() + ',Nuevo León, México'
+        deliveries_df['numerodecasa'].astype(int).astype(str).str.strip() + ',' + \
+        deliveries_df['departamento'].str.strip() + ',' + \
+        deliveries_df['nombreciudad'].str.strip().str.lower() + ',' + \
+        deliveries_df['num_codigopostal'].astype(int).astype(
+            str).str.strip() + ',Nuevo León, México'
 
     # Keep only deliveries, not returns
     deliveries_df = deliveries_df[deliveries_df['cantidad'] > 0]
 
     # Save to csv
-    deliveries_df.to_csv(PROCESSED_DATA_PATH + 'latest-deliveries.csv', index=False)
+    deliveries_df.to_csv(PROCESSED_DATA_PATH +
+                         'latest-deliveries.csv', index=False)
 
 
 def clean_vehicles(vehicles_db_path: str):
     vehicles_df = pd.read_excel(vehicles_db_path)
-    
+
     # Save it to csv
     vehicles_df.to_csv(PROCESSED_DATA_PATH + 'vehicles.csv', index=False)
 
+
 def clean_all_data():
-        
+
     clean_products('./data/raw/sku_DCF_Volumen.xls')
     clean_cedis('./data/raw/direcciones-Cedis-Coppel.xls')
     clean_deliveries('./data/raw/Monterrey-2021.csv')
