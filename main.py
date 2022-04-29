d096fadf55bcf76573f483426e38bb32766bdc28
Juan <pablowechg@gmail.com>
2022-04-29 08:45:03 -0500

    added main function to test the app

---
+++ main.py
@@ -0,0 +1,26 @@
+from src.solver import solver, visualize_routes
+import numpy as np
+
+
+def test(n):
+
+    distances = []
+    routes = []
+
+    for i in range(n):
+        route, distance = solver()
+        distances.append(distance)
+        routes.append(route)
+        print(distance)
+
+    # Return best route
+    distances = np.array(distances)
+    best_route = routes[np.argmin(distances)]
+    
+    return best_route, distances.min()
+
+
+if __name__ == '__main__':
+    best_route, distance = test(2)
+    print('Minimal distance:', distance)
+    visualize_routes(best_route)
