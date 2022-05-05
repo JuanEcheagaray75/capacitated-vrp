01a63e516ee929d658e9eb7537c752a6aaf4f2c4
Juan <pablowechg@gmail.com>
2022-05-04 19:16:58 -0500

    updated main code

--- main.py
+++ main.py
@@ -8,19 +8,26 @@ def test(n):
     routes = []
 
     for i in range(n):
+        print('-----------------')
         route, distance = solver()
         distances.append(distance)
         routes.append(route)
-        print(distance)
+        print(f'Distance_traveled: {distance}')
 
     # Return best route
     distances = np.array(distances)
     best_route = routes[np.argmin(distances)]
     
-    return best_route, distances.min()
+    n_clients = [len(i) - 1 for i in best_route]
+    
+    return best_route, distances.min(), n_clients
 
 
 if __name__ == '__main__':
-    best_route, distance = test(10)
-    print('Minimal distance:', distance / 100)
+    best_route, distance, n_clients = test(10)
+    print(f'Minimal distance: {distance / 100}')
+
+    for i in range(len(best_route)):
+        print(f'Car {i}: {n_clients[i]}')
+
     visualize_routes(best_route)
