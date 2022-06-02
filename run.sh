7932596769e7408e1a26fa033675ebd3f3e51d80
Juan <pablowechg@gmail.com>
2022-06-02 07:23:18 -0500

    chore: leave run script as should be

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-echo "Hello, user. Welcome to our optimization application, let's begin, shall we?"
-python3 src/data-cleaning.py
-python3 src/address-processing.py
-python3 main.py
+# echo "Hello, user. Welcome to our optimization application, let's begin, shall we?"
+#python3 src/data-cleaning.py
+#python3 src/address-processing.py
+#python3 main.py
 explorer.exe routes.html
